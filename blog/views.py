333ede1d34f2ad6540709b2b0774a67b6ce9a541
rejianedias <rejiane.dsilva@gmail.com>
2025-07-24 04:47:51 -0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -9,11 +9,11 @@ from django.shortcuts import redirect
 
 def post_list(request): 
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'cursos/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'cursos/post_detail.html', {'post': post})
 
 def post_new(request):
     if request.method == "POST":
@@ -25,7 +25,7 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'cursos/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
      post = get_object_or_404(Post, pk=pk)
@@ -39,4 +39,4 @@ def post_edit(request, pk):
              return redirect('post_detail', pk=post.pk)
      else:
          form = PostForm(instance=post)
-     return render(request, 'blog/post_edit.html', {'form': form})
+     return render(request, 'cursos/post_edit.html', {'form': form})
